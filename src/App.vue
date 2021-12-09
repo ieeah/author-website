e7ed343402a48eff8557c59bc267ac9c2d5dea92
ieeah <lbnfnc90@gmail.com>
2021-12-09 18:35:12 +0100

    done audiobook platforms banner

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
 	<div id="app">
 		<header>
 			<Head />
+			<main>
+				<Audiobook />
+			</main>
 		</header>
 	</div>
 </template>
 
 <script>
 import Head from '@/components/Head.vue';
+import Audiobook from '@/components/Audiobook.vue';
 export default {
 	name: 'App',
 	components: {
 		Head,
-
+		Audiobook,
 	}
 }
 </script>
---
+++ src/components/Audiobook.vue
@@ -0,0 +1,38 @@
+<template>
+	<section class="audiobooks">
+		<div class="platforms">
+			<div class="container sm">
+				<img :src="require('@/assets/audiobookHero/ebay-logotan-2x-200x66.png')" alt="">
+				<img :src="require('@/assets/audiobookHero/audible-logotan-2x-200x66.png')" alt="">
+				<img :src="require('@/assets/audiobookHero/kindlefire-logotan-2x-200x66.png')" alt="">
+				<img :src="require('@/assets/audiobookHero/bb-logotan-2x-200x66.png')" alt="">
+			</div>
+		</div>
+		<div class="hero">
+			<div class="container sm">
+
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Audiobook',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/globals.scss';
+.audiobooks {
+	.platforms {
+		background-color: $bg-2;
+		height: 200px;
+		.container.sm {
+			height: 100%;
+			@include flexAlignCenter;
+			justify-content: space-around;
+		}
+	}
+}
+</style>
