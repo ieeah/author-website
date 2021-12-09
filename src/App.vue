1414dee0acd2cda5bc0fcb8610fbc3daa3392712
ieeah <lbnfnc90@gmail.com>
2021-12-09 13:03:12 +0100

    starting to layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<header>
+			<Head />
+		</header>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Head from '@/components/Head.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Head,
+
+	}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/styles/globals.scss';
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="head">
+		<nav></nav>
+		<div class="hero"></div>
+		<div class="more_about"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Head',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
