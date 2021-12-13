d571bdcb622ecdeba315cc4bc8e3884acbc25399
ieeah <lbnfnc90@gmail.com>
2021-12-13 11:44:02 +0100

    styling cards

--- src/components/DeeperCard.vue
+++ src/components/DeeperCard.vue
@@ -1,15 +1,46 @@
 <template>
-	<div class="deeper_card rounded_bt_right">
-		<div class="heading"></div>
+	<div class="deeper_card">
+		<img :src="img" :alt="title">
+		<div class="content">
+			<h4 class="clear event_heading fw500">{{title}}</h4>
+			<div class="line"></div>
+			<div class="text accent">{{text}}</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-
+	name: 'DeeperCard',
+	props: {
+		img: String,
+		title: String,
+		text: String,
+	},
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import '@/styles/globals.scss';
 
+.deeper_card {
+	width: calc((100% - 150px) / 3);
+	margin-inline: 25px;
+	height: 512px;
+	background-color: red;
+	position: relative;
+	img {
+		@include full100;
+		object-fit: cover;
+		object-position: left;
+	}
+	.content {
+		@include absoluteTopLeft;
+		text-align: center;
+		@include full100;
+		padding-top: 80%;
+		background: rgb(0,0,0);
+		background: linear-gradient(0deg, rgba(0,0,0,.6) 10%, rgba(0,0,0,0.2) 60%);
+	}
+}
 </style>
--- src/components/DigDeeper.vue
+++ src/components/DigDeeper.vue
@@ -1,38 +1,48 @@
 <template>
-	<section id="dig_deeper" class="container lg ">
+	<section id="dig_deeper" class="container md">
 		<div class="three_cards_heading">
 			<h2 class="section_heading primary">Dig A Little Deeper Into Damon Vaughn</h2>
 			<h5 class="subtitle third">find out more about the author himself</h5>
 		</div>
 		<div class="deeper_articles">
-			
+			<DeeperCard 
+				class="card rounded_bt_right"
+				v-for="(card, i) in deeperCards" :key="`card_${i}`"
+				:img="deeperCards[i].img"
+				:title="deeperCards[i].title"
+				:text="deeperCards[i].text"
+			/>
 		</div>
 	</section>
 </template>
 
 <script>
+import DeeperCard from '@/components/DeeperCard.vue';
 export default {
 	name: 'DigDeeper',
 	props: {
 		cards: Object,
+	},
+	components: {
+		DeeperCard,
 	},
 	data() {
 		return {
 			deeperCards: [
 				{
 					img: require('@/assets/digDeeper/box-1.jpg'),
-					title: '',
-					text: '',
+					title: 'About Me',
+					text: 'Vestibulum ante ipsum primis',
 				},
 				{
 					img: require('@/assets/digDeeper/box-2.jpg'),
-					title: '',
-					text: '',
+					title: 'My Latest Book',
+					text: 'Vestibulum ante ipsum primis',
 				},
 				{
 					img: require('@/assets/digDeeper/box-3.jpg'),
-					title: '',
-					text: '',
+					title: 'Book Signing',
+					text: 'Vestibulum ante ipsum primis',
 				},
 			],
 		};
@@ -45,7 +55,9 @@ export default {
 
 #dig_deeper {
 	padding-block: 60px;
-	@include flexBetween;
+	.deeper_articles {
+		@include flexBetween;
+	}
 }
 
 </style>
